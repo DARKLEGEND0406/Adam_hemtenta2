ec94b55598a2bc2b10587d932e7e684d8d794b4f
Adam Hammadi <adam.hammadi@elev.ga.ntig.se>
2023-11-07 14:41:59 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Produkthantering</title>
+</head>
+<body>
+    <h1>Produkthantering</h1>
+    <ul>
+        <li><a href="./functions/add_product.php">Lägg till produkt</a></li>
+        <li><a href="./functions/view_products.php">Se alla produkter</a></li>
+        <li><a href="./functions/edit_product.php">Ändra pris/bild på produkt</a></li>
+        <li><a href="./functions/delete_product.php">Ta bort produkt</a></li>
+    </ul>
+</body>
+</html>
