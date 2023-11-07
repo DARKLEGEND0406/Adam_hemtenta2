ba344bed807aff80c383e705ba1f9612c4ac15e9
Adam Hammadi <adam.hammadi@elev.ga.ntig.se>
2023-11-07 14:53:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Produkthantering</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adam_hemtenta2</title>
 </head>
 <body>
-    <h1>Produkthantering</h1>
     <ul>
         <li><a href="./functions/add_product.php">Lägg till produkt</a></li>
         <li><a href="./functions/view_products.php">Se alla produkter</a></li>
         <li><a href="./functions/edit_product.php">Ändra pris/bild på produkt</a></li>
         <li><a href="./functions/delete_product.php">Ta bort produkt</a></li>
     </ul>
+    
 </body>
 </html>
