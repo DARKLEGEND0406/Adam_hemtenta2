fa991b349a2606c9b583267524a86a854c7cf061
Adam Hammadi <adam.hammadi@elev.ga.ntig.se>
2023-11-07 21:54:52 +0300

    1

--- index.php
+++ index.php
@@ -3,15 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adam_hemtenta2</title>
+    <title>Document</title>
 </head>
 <body>
-    <ul>
-        <li><a href="./functions/add_product.php">Lägg till produkt</a></li>
-        <li><a href="./functions/view_products.php">Se alla produkter</a></li>
-        <li><a href="./functions/edit_product.php">Ändra pris/bild på produkt</a></li>
-        <li><a href="./functions/delete_product.php">Ta bort produkt</a></li>
-    </ul>
+
+    
     
 </body>
 </html>
